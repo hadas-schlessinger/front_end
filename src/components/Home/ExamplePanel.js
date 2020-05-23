1281bd9d0fbf51a73da84a9b40dfb669a1b80287
Hadas Schlessinger <hadass@rezilion.com>
2020-05-23 17:51:04 +0300

    add explanations

--- src/components/Home/ExamplePanel.js
+++ src/components/Home/ExamplePanel.js
@@ -6,7 +6,7 @@ export default function ExamplePanel() {
         <div style={{backgroundImage: `url(${beckgroungTransperant})`}}>
             <h1>Examples</h1>
             <h2>Jupiter notebook</h2>
-            <p style={{fontSize: 20}}> This iד a jupiter notebook demonstrating the method execution:</p>
+            <p style={{fontSize: 20}}> This is a jupiter notebook demonstrating the method execution:</p>
             <p style={{fontSize: 20}}> <a href="https://nbviewer.jupyter.org/github/liel-cohen/CytoMod/blob/master/cytomod_notebook.ipynb">The Jupiter Notebook Example</a></p>  
         
         </div>
