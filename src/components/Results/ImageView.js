19a2399c670d8cad5328d1dbade656ffb568ecc3
Hadas Schlessinger <hadass@rezilion.com>
2020-06-06 11:59:16 +0300

    bolded explanation

--- src/components/Results/ImageView.js
+++ src/components/Results/ImageView.js
@@ -11,7 +11,8 @@ export default function ImageView(props) {
                 <tr>
                 <td>
                 <h3>Image: {props.results.headline[props.row]}</h3>
-                <p>Explanation: {props.results.explanation[props.row]}</p>
+                <p><b>Explanation:</b>
+                {props.results.explanation[props.row]}</p>
                 <a download = {String(props.results.headline[props.row]) + '.png'} href= {decoder(props.results.image[props.row])} title={props.results.headline[props.row]} >                    
                 <img src = {decoder(props.results.image[props.row])} width = {props.results.width[props.row]} height = {props.results.height[props.row]}/></a>
                 </td>
