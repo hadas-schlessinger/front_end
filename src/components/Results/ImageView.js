5af7e29137a2219b14f47a1e5e963155bce6fc66
Hadas Schlessinger <hadass@rezilion.com>
2020-07-02 21:50:30 +0300

    image view changes

--- src/components/Results/ImageView.js
+++ src/components/Results/ImageView.js
@@ -13,13 +13,11 @@ export default function ImageView(props) {
                 <h3>Figure: {props.results.headline[props.row]}</h3>
                 <a download = {String(props.results.headline[props.row]) + '.png'} href= {decoder(props.results.image[props.row])} title={props.results.headline[props.row]} >                    
                 <img src = {decoder(props.results.image[props.row])} width = {props.results.width[props.row]} height = {props.results.height[props.row]}/></a>
+                <p>
+                {props.results.explanation[props.row]}</p>
                 <p> </p>
-                <b>
-                {props.results.explanation[props.row]}</b>
                 </td>
-                </tr>
-    
-                
+                </tr>             
                 </React.Fragment>
 
         )
